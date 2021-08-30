2579924e5cb9621fb977dabcecb7e451800511ba
Artur Wojtczak <arturwojtczak1993@gmail.com>
2021-08-30 11:12:59 +0200

    secend commit

---
+++ src/main/java/pl/wojtczak/lesson5/App.java
@@ -0,0 +1,29 @@
+//package pl.wojtczak.lesson5;
+//
+//import pl.wojtczak.lesson4.example.cars.Car;
+//
+//public class App {
+//    public static void main( String[] args ) {
+//    Car myCar = new Car();
+//    Car yourCar = new Car();
+//    Car car3 = myCar;
+//
+//    myCar.drive(50);
+//        System.out.println("MyCar " + myCar.drive(50));
+//        System.out.println("Car3 " + car3.drive(50));
+//        System.out.println("YourCar" + yourCar.drive(50));
+//
+//        car3 = yourCar;
+//
+//        yourCar.drive(100);
+//        System.out.println("car 3 after yourCar:" + car3.drive(0));
+//    }
+//        Car myCar = new Car();
+//        Car bmw = new Car(4,"BMW","3");
+//        Car audi;
+//
+//        System.out.println(bmw.mileage);
+//        System.out.println(bmw.doors);
+//        System.out.println(bmw.brand);
+//        System.out.println(bmw.model);
+//    }}
