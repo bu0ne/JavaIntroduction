982617ed32d73afec6262760878a180067117626
Mateusz Marczak <mateusz.marczak@empik.com>
2021-08-31 20:34:55 +0200

    second commit from Mateusz

--- src/main/java/pl/wojtczak/lesson4/example/hospital/Main.java
+++ src/main/java/pl/wojtczak/lesson4/example/hospital/Main.java
@@ -2,11 +2,11 @@ package pl.wojtczak.lesson4.example.hospital;
 
 public class Main {
   public static void main(String[] args) {
-    Doctor doctor = new Doctor();
-    doctor.setName("Artur");
-    doctor.setSurname("Wojtczak");
-    doctor.specialization = "cardiopedriat";
-    doctor.license = "1341";
+    Doctor dorothy = new Doctor();
+    dorothy.setName("Artur");
+    dorothy.setSurname("Wojtczak");
+    dorothy.specialization = "cardiopedriat";
+    dorothy.license = "1341";
 
     Nurse nurse = new Nurse();
     nurse.name = "helena";
@@ -17,8 +17,8 @@ public class Main {
     employee.name = "Jan";
     employee.surname = "Nowak";
 
-    System.out.println(doctor.name);
-    System.out.println(doctor.name);
+    System.out.println(dorothy.name);
+    System.out.println(dorothy.name);
   }
 
 }
