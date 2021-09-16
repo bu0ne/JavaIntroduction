72623319d9d704f75155565fb6a715d682313410
Artur Wojtczak <arturwojtczak1993@gmail.com>
2021-09-16 17:17:25 +0200

    Task 3

---
+++ src/main/java/pl/wojtczak/lesson7/homework/CountingCharacters.java
@@ -0,0 +1,13 @@
+package pl.wojtczak.lesson7.homework;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class CountingCharacters {
+
+    public static void main(String[] args) throws IOException {
+
+    System.out.println(Files.lines(Paths.get("data.txt")).mapToInt(String::length).sum());
+}
+}
