6cd986d358d81dbb7ca2a64c6bf115e84c0de738
Artur Wojtczak <arturwojtczak1993@gmail.com>
2021-09-18 12:30:42 +0200

    Task 4

---
+++ src/main/java/pl/wojtczak/lesson2/homework/AgeVerification.java
@@ -0,0 +1,12 @@
+package pl.wojtczak.lesson2.homework;
+
+public class AgeVerification {
+
+    public boolean isAdult(int age) {
+        if (age >= 18) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
