f8c344d3299e6cb05643d287b2d3c7a993f4a93f
Artur Wojtczak <arturwojtczak1993@gmail.com>
2021-09-18 12:15:03 +0200

    Task 3

---
+++ src/main/java/pl/wojtczak/lesson2/homework/Calculator.java
@@ -0,0 +1,22 @@
+package pl.wojtczak.lesson2.homework;
+
+public class Calculator {
+
+    public static int add (int num1, int num2){
+      return num1 + num2;
+    }
+    public static int sub (int num3, int num4){
+        return num3 - num4;
+    }
+    public static int multi (int num5, int num6){
+        return num5 * num6;
+    }
+    public static int divi (int num7, int num8){
+        return num7 / num8;
+    }
+    public static int modulo (int num9, int num0){
+        return num9 % num0;
+    }
+
+
+}
