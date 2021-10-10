9308269c03276daa9726772c39cedda3bafb228a
Artur Wojtczak <arturwojtczak1993@gmail.com>
2021-10-10 21:11:34 +0200

    Task 7

---
+++ src/main/java/pl/wojtczak/lesson4/homework/DaysOfTheWeek.java
@@ -0,0 +1,11 @@
+package pl.wojtczak.lesson4.homework;
+
+public class DaysOfTheWeek {
+
+  static String getDay(EnumDays days){
+      return String.valueOf(days);
+
+
+  }
+}
+
---
+++ src/main/java/pl/wojtczak/lesson4/homework/EnumDays.java
@@ -0,0 +1,17 @@
+package pl.wojtczak.lesson4.homework;
+
+public class EnumDays {
+
+    public enum Days {
+
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday,
+
+    }
+}
+
