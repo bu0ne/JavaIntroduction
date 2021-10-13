10c35bbda26d57be1488efb3d65c42eaebfcfb5c
Artur Wojtczak <arturwojtczak1993@gmail.com>
2021-10-13 20:57:38 +0200

    Task 3

---
+++ src/main/java/pl/wojtczak/lesson5/homework/ImprovedCalc/Multiplication.java
@@ -0,0 +1,8 @@
+package pl.wojtczak.lesson5.homework.ImprovedCalc;
+
+public class Multiplication implements ImprovedCalculator {
+    @Override
+    public double operation(double num1, double num2) {
+        return num1 * num2;
+    }
+}
