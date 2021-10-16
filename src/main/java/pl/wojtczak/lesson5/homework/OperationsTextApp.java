80735897685dfab6b048a6578c51f6feee8020ad
Artur Wojtczak <arturwojtczak1993@gmail.com>
2021-10-16 10:40:00 +0200

    Task 8

---
+++ src/main/java/pl/wojtczak/lesson5/homework/OperationsTextApp.java
@@ -0,0 +1,14 @@
+package pl.wojtczak.lesson5.homework;
+
+import static pl.wojtczak.lesson5.homework.OperationsText.*;
+
+public class OperationsTextApp {
+
+    public static void main(String[] args) {
+
+        isEmpty("");
+        isPalindrome("peep");
+        lengthText("peep");
+
+    }
+}
